25e7525d2b12816e0e32df9381874057acfc4a3d
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-01-07 20:39:10 +0530

    test initiated

---
+++ Python/phw/ex2.py
@@ -0,0 +1,8 @@
+# Comments and pound character
+
+# comments are used to read later and anything after #key wont display
+print("There are so many methods to use '#' and that are following ") #i can use #like this it wont print
+
+#print("This is also wont print") #if i remove # and this line will also run
+
+print("This will print")
