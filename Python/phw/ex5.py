e44fd21074948e178acbe777df9c258042ba3b52
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-01-07 06:57:50 +0000

    phw Code initiated

---
+++ Python/phw/ex5.py
@@ -0,0 +1,17 @@
+#more variables
+my_name = 'Vijay'
+my_age = 24
+my_height = 5.9
+my_weight = 61 
+my_eyes = 'black'
+my_hair = 'black'
+
+
+print(f"lets talk about {my_name}.")
+print(f"He is {my_age} old")
+print(f"He is {my_height} feet")
+print(f"He is {my_weight} kg weight")
+print(f"He has {my_eyes} eyes")
+print(f"And he has {my_hair} hair")
+total = my_age + my_height + my_weight
+print(f"if i add {my_age} and {my_height} and {my_weight}, i will get {total}.")
---
+++ Python/phw/ex7.py
@@ -0,0 +1,23 @@
+#more printing
+
+print("Mary had a little lamb")
+print("Its fleence was white as {}" .format('snow')) #This is another way to use format here we can assign the snow inside the format print
+print("And everywhat that mary went")
+print('*' * 5 ) #what would that do #it will print 15 - after the last print statement
+
+end1 = 'H'
+end2 = 'e'
+end3 = 'l'
+end4 = 'l'
+end5 = 'o'
+end6 = ','
+end7 = 'W'
+end8 = 'o'
+end9 = 'r'
+end10 = 'l'
+end11 = 'd'
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+print(end7 + end8+ end9 + end10 +end11)
+
+# the last end ' ' is to ask the next print statemet to print after this not a next line
