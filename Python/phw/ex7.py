984b1619a1fb75162746b56a77d01bec6a018f66
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-01-08 21:06:13 +0530

    phw init

--- Python/phw/ex7.py
+++ Python/phw/ex7.py
@@ -1,9 +1,9 @@
 #more printing
 
 print("Mary had a little lamb")
-print("Its fleence was white as {}" .format('snow')) #This is another way to use format here we can assign the snow inside the format print
-print("And everywhat that mary went")
-print('*' * 5 ) #what would that do #it will print 15 - after the last print statement
+print("Its fleence was white as {}.".format('snow')) #This is another way to use format here we can assign the snow inside the format print
+print("And everywhat that mary went.")
+print("." * 5) #what would that do #it will print 15 - after the last print statement
 
 end1 = 'H'
 end2 = 'e'
@@ -18,6 +18,6 @@ end10 = 'l'
 end11 = 'd'
 
 print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
-print(end7 + end8+ end9 + end10 +end11)
+print(end7 + end8+ end9 + end10 + end11)
 
 # the last end ' ' is to ask the next print statemet to print after this not a next line
