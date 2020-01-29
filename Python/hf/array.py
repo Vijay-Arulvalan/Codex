ea06d15008cd895a2c373a516bc7e44c2ccb2cb4
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-01-29 14:35:58 +0000

    initiated

---
+++ Python/hf/array.py
@@ -0,0 +1,36 @@
+# Head First
+
+room = ["Vijay", "Ganesh", "Jidhu", "Thanos", "Vicky"]
+movies = ["The Holy Grail", 1975,
+         "The Life of Brian", 1979,
+         "The Meaning of Life", 1983]
+fav_movies = ["The Holy Grail", "The Life of Brian"]
+
+print(fav_movies[0])
+print(fav_movies[1])
+print(room)
+print(movies)
+print(len(room))
+print(room[2])
+
+room.append("Anand")
+print(room)
+
+room.pop()
+print(room)
+
+room.extend(["Anand", "Vignesh"])
+print(room)
+
+room.insert(4, "Aashi")
+print(room)
+
+room.remove("Aashi")
+print(room)
+
+
+room.insert(1, 1995) #or room = ["Vijay",1995,                              "Ganesh", "Jidhu",1996, "Thanos"                      "vicky", 1997, "Anand",                                "Vignesh", 1996]
+room.insert(4, 1996)
+room.append(1996)
+print(room)
+
