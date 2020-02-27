50267a7fa16608151b863a898d353766d6456daf
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-02-27 18:18:28 +0530

    crash init

---
+++ Python/crash/amushment.py
@@ -0,0 +1,21 @@
+age = 17
+
+if age <= 4:
+    print("Your admission cost is free!")
+elif age <18:
+    print("your admission cost is $5. ")
+else:
+    print("your admission cost is $10.")
+
+#Another methos
+
+age = 17
+
+if age <=4:
+    price = 0
+elif age <18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission price is $" + str(price) + ".")
---
+++ Python/crash/condition.py
@@ -0,0 +1,7 @@
+car = 'subaru'
+
+print("Is car == 'audi'? I predict True. ")
+print(car == 'audi')
+
+print("\n Is car == 'subaru' I predict False. ")
+print(car == 'subaru')
