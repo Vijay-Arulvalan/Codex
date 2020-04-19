af7b711ac2bbdf682f2858db1a46d1cb2f538c97
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-04-19 16:22:26 +0530

    file init x

--- Python/crash/json_refactorx.py
+++ Python/crash/json_refactorx.py
@@ -11,15 +11,21 @@ def get_stored_info():
         return None
     else:
         return username
+def get_username():
+    """prompt for a new username"""
+    username = input ("What's your name: ")
+    filename = 'user.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
 def greet_user():
     """Greet the user by name"""
     username = get_stored_info()
     if username:
-        print('Welcome back ' + username)
+        print('Welcome back ' + username + '.')
     else:
-        username = input("What's your name: ")
-        filename = 'info.json'
-        with open(filename, 'w') as f_obj:
-            json.dump(username, f_obj)
-            print('We will remember you, when you come back ' + username + '.')
+        username = get_username()
+        print('We will remember you when you come back ' + username + '.')
+
 greet_user()
